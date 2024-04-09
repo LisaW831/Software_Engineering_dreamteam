054e4060e6e918e4c3446547e4f17f0a17fa2252
Gareth Rodger <40617672@live.napier.ac.uk>
2024-04-09 12:26:10 +0100

    Added World Java Class

---
+++ src/main/java/com/napier/sem/world.java
@@ -0,0 +1,126 @@
+package com.napier.sem;
+
+public class world
+{
+    /**
+     * Employee number
+     */
+    public int world_population;
+
+    /**
+     * Employee's last name
+     */
+    public String continent_name;
+
+    /**
+     * Employee's first name
+     */
+    public int continent_population;
+
+    /**
+     * Employee's first name
+     */
+    public int continent_pop_in_city;
+
+    /**
+     * Employee's first name
+     */
+    public int continent_pop_not_in_city;
+
+    /**
+     * Employee's last name
+     */
+    public String region_name;
+
+    /**
+     * Employee's first name
+     */
+    public int region_population;
+
+    /**
+     * Employee's first name
+     */
+    public int region_pop_in_city;
+
+    /**
+     * Employee's first name
+     */
+    public int region_pop_not_in_city;
+
+    /**
+     * Employee's last name
+     */
+    public String country_name;
+
+    /**
+     * Employee's first name
+     */
+    public int country_population;
+
+    /**
+     * Employee's first name
+     */
+    public int country_pop_in_city;
+
+    /**
+     * Employee's first name
+     */
+    public int country_pop_not_in_city;
+
+    /**
+     * Employee's last name
+     */
+    public String district_name;
+
+    /**
+     * Employee's first name
+     */
+    public int district_population;
+
+    /**
+     * Employee's last name
+     */
+    public String capital_city_name;
+
+    /**
+     * Employee's first name
+     */
+    public int capital_city_population;
+
+    /**
+     * Employee's last name
+     */
+    public String city_name;
+
+    /**
+     * Employee's first name
+     */
+    public int city_population;
+
+    /**
+     * Employee's first name
+     */
+    public int chinese;
+
+    /**
+     * Employee's first name
+     */
+    public int english;
+
+    /**
+     * Employee's first name
+     */
+    public int hindi;
+
+    /**
+     * Employee's first name
+     */
+    public int spanish;
+
+    /**
+     * Employee's first name
+     */
+    public int arabic;
+
+
+}
