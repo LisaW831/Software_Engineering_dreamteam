29dd0ca3ace6dddea04bd24fc950129ab30553dd
Gilleto123 <157487577+Gilleto123@users.noreply.github.com>
2024-04-20 07:37:12 +0100

    Update Report_23.java
    Comment added before class

--- src/main/java/com/napier/sem/Report_23.java
+++ src/main/java/com/napier/sem/Report_23.java
@@ -4,6 +4,7 @@ import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 
+// Class defined to create the report to query: The population of people, people living in cities, and people not living in cities in each continent.
 public class Report_23 {
     static Connection con = null;
 
