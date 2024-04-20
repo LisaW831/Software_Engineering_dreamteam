6d14910fbd0cf701a90b8ba1bf5db4b89600f07d
Gilleto123 <157487577+Gilleto123@users.noreply.github.com>
2024-04-20 07:31:35 +0100

    Update Report_17.java
    Comment added before class

--- src/main/java/com/napier/sem/Report_17.java
+++ src/main/java/com/napier/sem/Report_17.java
@@ -2,6 +2,7 @@ package com.napier.sem;
 
 import java.sql.*;
 
+// Class defined to create the report to query: All the capital cities in the world organised by largest population to smallest.
 public class Report_17 {
     static Connection con = null;
 
